2b88b4991cc9a48c8506b06c3e04dec01355d134
David Pulido <dpulidoc99@gmail.com>
2023-09-22 20:37:05 +0200

    compte rendu hidden

--- tests/merge_tester.sh
+++ tests/merge_tester.sh
@@ -13,13 +13,13 @@ sleep $duration
 echo test >> tests/test.txt
 valgrind -s --leak-check=full ./bin/main add tests/test.txt
 sleep $duration
-#valgrind -s --leak-check=full ./bin/main add tests/test2.txt
+valgrind -s --leak-check=full ./bin/main add tests/test2.txt
 sleep $duration
 valgrind -s --leak-check=full ./bin/main commit test -m commit_1_test
 sleep $duration
 valgrind -s --leak-check=full ./bin/main checkout-branch master
 sleep $duration
-#valgrind -s --leak-check=full ./bin/main add tests/test_wt.txt
+valgrind -s --leak-check=full ./bin/main add tests/test_wt.txt
 sleep $duration
 echo master >> tests/test.txt
 sleep $duration
